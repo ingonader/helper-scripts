39b2231e0267e068464daaa00a146198448a9010
Ingo Nader <ingo.nader@it-ps.at>
2019-11-19 10:33:13 +0100

    Update documentation

--- close-and-focus.py
+++ close-and-focus.py
@@ -24,6 +24,7 @@ def get_client_stacking():
   
   Returns a list of the windows in the order that they were last actively used.
   Returns None if the client stacking list was not found.
+  Uses `xprop` under the hood.
   
   Parameters: None.
   
@@ -40,30 +41,48 @@ def get_client_stacking():
     ret = None
   return(ret)
 
-#get_client_stacking()
-
-
 
 def get_active_window_id():
-  ## get active window ID via xprop:
+  """
+  Get ID of active window
+  
+  Uses `xprop` command line tools to get the ID of the currently active window in KDE. 
+  
+  Paramters: None.
+  
+  Returns: 
+  string: hexadeximal ID of currently active window, with 8 digits (e.g., '0x0220000a')
+  """
+  ## get active window ID via xprop and convert to utf-8:
   win_id_byte = subprocess.check_output(["/usr/bin/xprop", "-root", "_NET_ACTIVE_WINDOW"])
   win_id_str = win_id_byte.decode("utf-8")
-  ## also possible: subprocess.check_call; subprocess.call;
-  ## don't use os.system any longer:
-  ## # win_id = os.system("xprop -root _NET_ACTIVE_WINDOW")
   
   ## extract active window ID from results string:
-  #re.search(r'.*?: window id # ', win_id_str)
   win_id = win_id_str
   win_id = re.sub(r"^.*?: window id # ", "", win_id)
   win_id = re.sub(r"\n", "", win_id)
+  
   ## add leading zero to conform to wmctrl output (8-digit hex):
   win_id = re.sub(r"0x", "0x0", win_id)
   return(win_id)
 
 
-## note: function returns pandas dataframe without an index without any information (index only contains only an enumeration)
 def get_wmctrl_data():
+  """
+  Gets currently open windows
+  
+  Uses the `wmctrl` command line tool (with the `-lx` option) to list all 
+  currently open windows (IDs, names and some other data).
+  note: function returns pandas dataframe without an index without any 
+  information (index only contains only an enumeration).
+  
+  Parameters: None.
+  
+  Returns:
+  pandas dataframe: One row per currently open application window, with the
+    following columns: 
+    ['win_id', 'win_status', 'win_type', 'win_os', 'win_name']
+  """
   ## get window wmctrl output and convert to utf-8:
   wmctrl_byte = subprocess.check_output(["/usr/bin/wmctrl", "-lx"])
   wmctrl_str = wmctrl_byte.decode("utf-8")
@@ -83,12 +102,7 @@ def get_wmctrl_data():
     index_col = False)
   return(dat_wmctrl)
 
-# dat_wmctrl = get_wmctrl_data()
-# dat_wmctrl.shape
 
-
-#def get_client_stacking():
-  
 
 ## ========================================================================= ##
 ## main
