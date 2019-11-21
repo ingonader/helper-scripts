81d2279cd445297f6bf4e3fc654d8eb9f3847190
Ingo Nader <ingo.nader@it-ps.at>
2019-11-21 10:25:30 +0100

    Add check if python script is "__main__"

--- close-and-focus.py
+++ close-and-focus.py
@@ -145,28 +145,30 @@ def get_active_windows(sort = True):
 ## main
 ## ========================================================================= ##
 
-## get active window:
-win_id = get_active_window_id()
+if __name__ == "__main__":
+  ## get active window:
+  win_id = get_active_window_id()
+  
+  ## get active windows, sorted by recent activity:
+  dat_wmctrl = get_active_windows(sort = True)
+  
+  ## get application type of active window id:
+  win_type = dat_wmctrl[dat_wmctrl["win_id"] == win_id]["win_type"].values[0]
+  
+  ## close active window:
+  subprocess.run(["/usr/bin/wmctrl", "-c", ":ACTIVE:"])
+  
+  ## remove active application from window list:
+  dat_wmctrl = dat_wmctrl[dat_wmctrl["win_id"] != win_id]
+  
+  ## find next remaining instance of active application:
+  dat_wmctrl_active = dat_wmctrl[dat_wmctrl["win_type"] == win_type]
+  
+  ## if there is one, focus it (e.g., wmctrl -i -a 0x00200007):
+  if dat_wmctrl_active.shape[0] > 0:
+    next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
+    outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
 
-## get active windows, sorted by recent activity:
-dat_wmctrl = get_active_windows(sort = True)
-
-## get application type of active window id:
-win_type = dat_wmctrl[dat_wmctrl["win_id"] == win_id]["win_type"].values[0]
-
-## close active window:
-subprocess.run(["/usr/bin/wmctrl", "-c", ":ACTIVE:"])
-
-## remove active application from window list:
-dat_wmctrl = dat_wmctrl[dat_wmctrl["win_id"] != win_id]
-
-## find next remaining instance of active application:
-dat_wmctrl_active = dat_wmctrl[dat_wmctrl["win_type"] == win_type]
-
-## if there is one, focus it (e.g., wmctrl -i -a 0x00200007):
-if dat_wmctrl_active.shape[0] > 0:
-  next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
-  outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
   
 
   
