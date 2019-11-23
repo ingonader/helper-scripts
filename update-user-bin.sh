cdd2ddeaadaa0207e449c66d7d35214a32a3fdb4
Ingo Nader <ingo.nader@it-ps.at>
2019-11-23 21:33:40 +0100

    Add small bash script to copy files to ~/bin folder

---
+++ update-user-bin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo copying linux_wmctrl_fnlib.py to ~/bin/...
+cp ./linux_wmctrl_fnlib.py ~/bin/
+
+echo copying close-and-focus.py to ~/bin/...
+cp ./close-and-focus.py ~/bin/
+
+echo done.
