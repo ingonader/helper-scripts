3a728e67f856d0600a43da51c08cce0ace9889b8
Ingo Nader <ingo.nader@it-ps.at>
2020-02-17 21:35:50 +0100

    Improve code styling

--- close-and-focus.py
+++ close-and-focus.py
@@ -15,30 +15,27 @@ from linux_wmctrl_fnlib import *
 ## ========================================================================= ##
 
 if __name__ == "__main__":
-  ## get active window:
-  win_id = get_active_window_id()
-  
-  ## get active windows, sorted by recent activity:
-  dat_wmctrl = get_active_windows(sort = True)
-  
-  ## get application type of active window id:
-  active_application = dat_wmctrl[dat_wmctrl["win_id"] == win_id]\
-    ["application"].values[0]
-  
-  ## close active window:
-  subprocess.run(["/usr/bin/wmctrl", "-c", ":ACTIVE:"])
-  
-  ## remove active application from window list:
-  dat_wmctrl = dat_wmctrl[dat_wmctrl["win_id"] != win_id]
-  
-  ## find next remaining instance of active application:
-  dat_wmctrl_active = dat_wmctrl[dat_wmctrl["application"] == active_application]
-  
-  ## if there is one, focus it (e.g., wmctrl -i -a 0x00200007):
-  if dat_wmctrl_active.shape[0] > 0:
-    next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
-    outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
+    ## get active window:
+    win_id = get_active_window_id()
 
-  
+    ## get active windows, sorted by recent activity:
+    dat_wmctrl = get_active_windows(sort=True)
 
-  
+    ## get application type of active window id:
+    active_application = dat_wmctrl[dat_wmctrl["win_id"] == win_id][
+        "application"
+    ].values[0]
+
+    ## close active window:
+    subprocess.run(["/usr/bin/wmctrl", "-c", ":ACTIVE:"])
+
+    ## remove active application from window list:
+    dat_wmctrl = dat_wmctrl[dat_wmctrl["win_id"] != win_id]
+
+    ## find next remaining instance of active application:
+    dat_wmctrl_active = dat_wmctrl[dat_wmctrl["application"] == active_application]
+
+    ## if there is one, focus it (e.g., wmctrl -i -a 0x00200007):
+    if dat_wmctrl_active.shape[0] > 0:
+        next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
+        outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
--- start-or-focus.py
+++ start-or-focus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 ## ######################################################################### ##
-## script to start an application or focus the most recently used 
+## script to start an application or focus the most recently used
 ## window of that application
 ## ######################################################################### ##
 
@@ -19,17 +19,24 @@ import sys
 
 ## read command line arguments:
 parser = argparse.ArgumentParser(
-  description = "Start an application or focus the most recently used " +
-  "window of that application")
-parser.add_argument("application_binary", type = str, 
-  help = "Command to start the application from the command line")
-parser.add_argument("window_name", type = str,
-  help = "Name of the window as listed by `wmctrl -lx`. " + 
-  "Can be the full name of the window (e.g., 'Msgcompose.Thunderbird' " + 
-  "or 'Mail.Thunderbird') to only match specific windows " +
-  "of an application, " + 
-  "or just the second part of that string to find any window of " + 
-  "that application (e.g., 'Thunderbird')")
+    description="Start an application or focus the most recently used "
+    + "window of that application"
+)
+parser.add_argument(
+    "application_binary",
+    type=str,
+    help="Command to start the application from the command line",
+)
+parser.add_argument(
+    "window_name",
+    type=str,
+    help="Name of the window as listed by `wmctrl -lx`. "
+    + "Can be the full name of the window (e.g., 'Msgcompose.Thunderbird' "
+    + "or 'Mail.Thunderbird') to only match specific windows "
+    + "of an application, "
+    + "or just the second part of that string to find any window of "
+    + "that application (e.g., 'Thunderbird')",
+)
 args = parser.parse_args()
 
 ## debug:
@@ -42,33 +49,32 @@ dot_position = args.window_name.find(".")
 
 ## extract window type and application:
 if dot_position == -1:
-  win_type = None
-  application = args.window_name
+    win_type = None
+    application = args.window_name
 else:
-  win_type = args.window_name
-  application = args.window_name[(dot_position + 1):]
+    win_type = args.window_name
+    application = args.window_name[(dot_position + 1) :]
 
 ## get active windows, sorted by recent activity:
-dat_wmctrl = get_active_windows(sort = True)
+dat_wmctrl = get_active_windows(sort=True)
 
 ## check if specific window type is already open:
 dat_wmctrl_active = dat_wmctrl[(dat_wmctrl["win_type"] == win_type)]
 
 ## if so, focus that window and exit:
 if dat_wmctrl_active.shape[0] > 0:
-  next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
-  outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
-  sys.exit()
+    next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
+    outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
+    sys.exit()
 
 ## check if another window of that application is already open:
 dat_wmctrl_active = dat_wmctrl[dat_wmctrl["application"] == application]
 
 ## if so, focus that window and exit:
 if dat_wmctrl_active.shape[0] > 0:
-  next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
-  outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
-  sys.exit()
+    next_window = dat_wmctrl_active[0:1]["win_id"].values[0]
+    outp_byte = subprocess.run(["wmctrl", "-i", "-a", next_window])
+    sys.exit()
 
 ## if not, run application binary to start a new instance:
 subprocess.run([args.application_binary])
-
